f10b5d9a2bb2bfd83f654539fb4f75d14794dafa
Yeison Franco <ydfrancor@siesa.com.co>
2024-06-27 14:42:42 -0500

    Seeder v1

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -9,10 +9,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 //builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DockerConnection"));
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 
+builder.Services.AddTransient<SeedDb>();
+
 var app = builder.Build();
+
+/**********Seeder*******************/
+SeedData(app);
+void SeedData(WebApplication app)
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using (IServiceScope? scope = scopedFactory!.CreateScope())
+    {
+        SeedDb? service = scope.ServiceProvider.GetService<SeedDb>();
+        service!.SeedAsync().Wait();    // Wait() es otra forma de llamar un método asíncrono en lugar de usar "await"
+    }
+}
+/***********************************/
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
